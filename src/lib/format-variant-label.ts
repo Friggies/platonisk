3921f4f311e9198bd8d38907e80c93294d3f84d1
Friggies <85342562+Friggies@users.noreply.github.com>
2025-04-29 09:55:33 +0200

    updated sizemap

--- src/lib/format-variant-label.ts
+++ src/lib/format-variant-label.ts
@@ -2,17 +2,19 @@ export const formatVariantLabel = (variantName: string): string => {
   const [, size] = variantName.split(' / ');
 
   const sizeMap: Record<string, string> = {
-    xs: 'Ekstra Lille',
-    s: 'Lille',
-    m: 'Medium',
-    l: 'Stor',
-    xl: 'Ekstra Stor',
-    '2xl': '2x Ekstra Stor',
+    xs: 'Størrelse: XS',
+    s: 'Størrelse: S',
+    m: 'Størrelse: M',
+    l: 'Størrelse: L',
+    xl: 'Størrelse: XL',
+    '2xl': 'Størrelse: 2XL',
+    '3xl': 'Størrelse: 3XL',
+    '4xl': 'Størrelse: 4XL',
   };
 
   const lowerSize = size?.toLowerCase();
   if (sizeMap[lowerSize]) {
     return sizeMap[lowerSize];
   }
-  return size ? size : 'Størrelsesløs';
+  return size ? size : 'Størrelse: OS';
 };
