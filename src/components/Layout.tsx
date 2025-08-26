6f7a407646e1273202b09891f0b5fb78b6dfb400
Frej Dahl Linneke <85342562+Friggies@users.noreply.github.com>
2025-08-26 06:43:02 +0200

    close menu on page nav

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,25 @@
 import Header from './global/large/header/Header';
 import Footer from './global/large/footer/Footer';
 import Main from './global/small/main/Main';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Menu from './global/large/menu/Menu';
 
 const Layout = ({ children }) => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setMenuOpen(false);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [router]);
 
   return (
     <>
